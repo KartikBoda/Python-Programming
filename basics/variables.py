6d579fb2112033ddf13d812c3e14cd42a370b8f1
KartikBoda <kartik.boda@vit.edu.in>
2026-08-31 21:47:09 +0530

    Create variables.py

---
+++ basics/variables.py
@@ -0,0 +1,15 @@
+name = "Kartik"
+age = 19
+cgpa = 9.41
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("CGPA:", cgpa)
+print("Student:", is_student)
+
+print("\nData Types:")
+print(type(name))
+print(type(age))
+print(type(cgpa))
+print(type(is_student))
